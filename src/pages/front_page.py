0c6b1c9d5ef78774fd56187bdb511b9765323a1b
rjbaicich <rjbaicich@gmail.com>
2023-06-08 18:20:58 -0400

    push major update

--- src/pages/front_page.py
+++ src/pages/front_page.py
@@ -6,10 +6,13 @@ st.text('Is That A Clean Beach?')
 st.header('Here are the different pages of my application:')
 
 st.subheader('Your Beach Is')
-st.text('Your Beach Is: Returns an image to let you know if your beach is clean or not.')
-
+st.text('Returns an image to let you know if your Beach is Clean or Not.')
+st.image('https://www.rd.com/wp-content/uploads/2020/04/tropic-of-cancer-beach-in-exuma-bahamas.jpg?fit=700,467')
+st.image('https://i.natgeofe.com/n/8c1dcd07-ebc0-4c31-8522-0f80df632983/01_beachplastic_photo-one_4x3.jpg')
 st.subheader('Summary')
-st.text('Summary: Is a summarization page of all the inner workings of my application.')
+st.text('Summary: Is a summarization page of my application.')
 
-st.subheader('Query')
-st.text('Query: Allows a user to enter the name of a New York beach and return if its considered clean or not.')
+st.subheader('Find Out')
+st.text('''Allows a user to enter the name of a New York Beach 
+                and return if its considered clean or not.
+        ''')
--- src/pages/summary.py
+++ src/pages/summary.py
@@ -1,13 +1,17 @@
 import streamlit as st
 
-st.title("Summary Page for Application")\
+st.title("Summary Page for Application")
 
 st.text("""
-        This application was designed to show our users that when they enter the name of a New York beach, they find out if its considered clean or not.
+        This application was designed to show our 
+        users that when they enter the name of a New York beach, 
+        they find out if its considered clean or not.
         """)
 
 st.image('https://www.rd.com/wp-content/uploads/2020/04/tropic-of-cancer-beach-in-exuma-bahamas.jpg?fit=700,467')
 
 st.write("""
-         It is important that we all do our part to help keep our beaches and oceans clean. 
+        It is important that we all do our part to help 
+        keep our Beaches and Oceans clean. 
+        It is vital to our survival. 
          """)
